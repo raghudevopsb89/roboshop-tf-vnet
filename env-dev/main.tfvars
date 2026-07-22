036770224831a4651ee360509a2adf1b0600715a
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2026-07-22 07:04:34 +0530

    terraform: update 2 file(s) (main.tfvars,main.tf)

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,3 +1,6 @@
 env           = "dev"
 location      = "Denmark East"
-address_space = ["10.20.0.0/24"]
+address_space = ["10.20.0.0/22"]
+# subnets = {
+#   app = "10.20"
+# }
--- main.tf
+++ main.tf
@@ -10,5 +10,20 @@ resource "azurerm_virtual_network" "main" {
   address_space       = var.address_space
 }
 
+# resource "azurerm_subnet" "example" {
+#   name                 = "example-subnet"
+#   resource_group_name  = azurerm_resource_group.main.name
+#   virtual_network_name = azurerm_virtual_network.main.name
+#   address_prefixes     = ["10.0.1.0/24"]
+#
+#   delegation {
+#     name = "delegation"
+#
+#     service_delegation {
+#       name    = "Microsoft.ContainerInstance/containerGroups"
+#       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+#     }
+#   }
+# }
 
 
