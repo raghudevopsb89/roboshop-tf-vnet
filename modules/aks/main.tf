79f73b3bf11ecbfbd0057c13d61fe7f0ec183d6e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2026-07-27 07:11:04 +0530

    terraform: update 1 file(s) (main.tf)

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,9 +1,10 @@
 resource "azurerm_kubernetes_cluster" "main" {
-  name                = "roboshop-${var.env}"
-  location            = var.rg_location
-  resource_group_name = var.rg_name
-  dns_prefix          = "roboshop-${var.env}"
-  kubernetes_version  = "1.35.4"
+  name                 = "roboshop-${var.env}"
+  location             = var.rg_location
+  resource_group_name  = var.rg_name
+  dns_prefix           = "roboshop-${var.env}"
+  kubernetes_version   = "1.35.4"
+  azure_policy_enabled = true
 
   default_node_pool {
     name           = "default"
